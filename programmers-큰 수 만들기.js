d587104a38f59261848c5e6d46e2754e0a73f48b
SEOKWOOPARK <67639846+SEOKWOOPARK@users.noreply.github.com>
2020-09-14 13:21:03 +0900

    Create programmers-큰 수 만들기.js
    탐욕법

---
+++ programmers-큰 수 만들기.js
@@ -0,0 +1,40 @@
+function solution(number, k) {
+    let StNum = String(number);
+    let num_length = StNum.length;
+    let stack = [];
+    let count = 0;
+    
+    for(let i = 0; i < num_length; i++){
+       let element = number[i];
+        if(stack.length === 0){
+           stack.push(element);
+           continue;
+        }
+        
+        while(stack[stack.length-1] < element){
+            stack.pop();
+            count++;
+            
+            if(count === k) {
+                return stack.join("") + number.substring(i, num_length);
+            }
+            
+            if(stack.length === 0 ){
+                break;
+            }
+            
+        }
+        
+        stack.push(element);
+        
+    }
+    
+    return stack.join("").substring(0, num_length-k);
+}
+
+
+// number는 "1924"처럼 문자열 안에 숫자가 붙어 있는 상황이다.
+// 하나의 간편한 팁을 알게 되었다.
+// String(숫자를 감싼 따옴표 문자열) => typeof는 문자열로 유지되면서
+// 마치 배열 [1, 9, 2, 4]처럼 인덱스를 이용해서 편하게 연산할 수 있다.
+// 이 문제의 경우 let StNum = String(number)처럼 쓸 수 있다.
