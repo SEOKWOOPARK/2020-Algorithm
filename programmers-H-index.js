3856b0b872e784edbcd6fbbe23fafa073fcb07d4
SEOKWOOPARK <67639846+SEOKWOOPARK@users.noreply.github.com>
2020-10-04 15:01:37 +0900

    Update programmers-H-index.js

--- programmers-H-index.js
+++ programmers-H-index.js
@@ -10,4 +10,5 @@ function solution(citations) {
     return Math.max(...result);
 }
 
-
+// h인덱스 of 정렬
+// 문제 이해 자체에 어려움이 있었다. h의 후보가 주어진 citations 배열 안의 원소들이라고 오해한 듯하다. 또한 '나머지 논문'이 h편 이하로 남았다가 아니라 'h번 이하로 인용'된 것이다.
